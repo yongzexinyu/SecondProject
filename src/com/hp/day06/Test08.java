c0e31ccfd5161ce49da461d763fe697bf937b80f
wanggaoyang <wgy001611@outlook.com>
2021-07-14 11:26:58 +0800

    Date日期类

--- src/com/hp/day06/Test08.java
+++ src/com/hp/day06/Test08.java
@@ -1,12 +1,41 @@
 package com.hp.day06;
 
 
+import java.util.HashMap;
+
 public class Test08 {
 
     public static void main(String[] args) {
-        String line = "{ \"username\":\"tangseng\", \"age\":\"500\"  } ";
+      //  String line = "{ \"username\":\"tangseng\", \"age\":\"500\"  } ";
+        HashMap<String,String> map=new HashMap<String,String>();
+        map.put("username","tangseng");
+        map.put("age","500");
+        QunJingRen a=new QunJingRen();
+        a.setUsername(map.get("username"));
+        a.setAge(map.get("age"));
+        System.out.println(a.getUsername());
+        System.out.println(a.getAge());
 
     }
 }
 
+ class QunJingRen{
+    private String username;
+    private String age;
 
+     public String getUsername() {
+         return username;
+     }
+
+     public void setUsername(String username) {
+         this.username = username;
+     }
+
+     public String getAge() {
+         return age;
+     }
+
+     public void setAge(String age) {
+         this.age = age;
+     }
+ }
